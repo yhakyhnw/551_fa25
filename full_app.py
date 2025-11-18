c9628d15cbb85e761660a92a8a5340f941973e07
yhakyhnw <andrewkim951@gmail.com>
2025-11-17 22:59:22 -0800

    Update full_app.py

--- full_app.py
+++ full_app.py
@@ -8,7 +8,7 @@ def homepage():
     st.markdown("Select a demo to run:")
 
     st.subheader("SQL Demo")
-    st.write("Description: This section will run the SQL-based demo for the project.")
+    st.write("The SQL demo uses a second-hand car data!\nA dummy dataset is also available for speedy testing!")
 
     st.button("Start SQL Demo", on_click = lambda: set_page("sql"))
 
