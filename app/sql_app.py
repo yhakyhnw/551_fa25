cf200e3dfdd06be45e46423e8e4b4b79d2fbf0f0
yhakyhnw <andrewkim951@gmail.com>
2025-11-19 14:04:05 -0800

    Update sql_app.py

--- app/sql_app.py
+++ app/sql_app.py
@@ -7,6 +7,8 @@ base_directory = os.path.dirname(os.path.abspath(__file__))
 root_directory = os.path.dirname(base_directory)
 packages_directory = os.path.join(root_directory, "packages")
 
+sys.path.append(packages_directory)
+
 from SQL_package import PSO  
 
 
